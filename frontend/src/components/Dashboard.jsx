134e43b21eef84269d9f3c26701de9e93dbfe950
vishnukoushikN0717 <105723560+vishnukoushikN0717@users.noreply.github.com>
2025-01-18 19:56:47 +0530

    Update Dashboard.jsx

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { supabase } from '../services/Supabase.jsx';
-import '../styles/Dashboard.css';
+import '../styles/DashBoard.css';
 
 const Dashboard = () => {
   const [name, setName] = useState('');
